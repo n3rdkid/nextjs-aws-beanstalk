de1f2157559ebc7116cc3e5360a03e6e41aa66a2
Saurav <sauravads123@gmail.com>
2021-07-28 11:57:10 +0545

    minor

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          AWS BeanStalk TEST
+          AWS BeanStalk TEST - 01
         </h1>
 
         <p className={styles.description}>
